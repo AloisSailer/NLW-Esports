dc128f8a98d15fca41ced1029910079844d57a20
Alois Sailer <alois_sailer@yahoo.com.br>
2022-09-15 08:55:48 -0300

    continuando web

--- Ignite/web/src/main.tsx
+++ Ignite/web/src/main.tsx
@@ -5,6 +5,5 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    {/* isso é um componente!!! */}
   </React.StrictMode>
 )
