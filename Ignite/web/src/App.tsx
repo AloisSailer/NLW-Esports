ef58581139dd7bfcb7cd32e9bf251fa972f8eaf4
Alois Sailer <alois_sailer@yahoo.com.br>
2022-09-16 22:54:08 -0300

    criado Banner

--- Ignite/web/src/App.tsx
+++ Ignite/web/src/App.tsx
@@ -3,6 +3,7 @@ import { GameBanner } from './components/GameBanner';
 
 import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -14,28 +15,15 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner />
-        <GameBanner />
-        <GameBanner />
-        <GameBanner />
-        <GameBanner />
-        <GameBanner />
-        
+        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
+        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={3} />
+        <GameBanner bannerUrl="/game-3.png" title="League of Legends" adsCount={5} />
+        <GameBanner bannerUrl="/game-4.png" title="League of Legends" adsCount={5} />
       </div>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
-            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
-          </div>
+      <CreateAdBanner />
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      
     </div>
   )
 }
